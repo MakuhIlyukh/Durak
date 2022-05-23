3a94e903630c9e4e3e6349d5ffb5519ee49b21b4
Makukha_Ilia <makukha.it@gmail.com>
2022-05-24 02:53:26 +0300

    Turn_2a_v1 was added

--- durak/envs.py
+++ durak/envs.py
@@ -1,4 +1,4 @@
-'''Мультиагентные среды для игры в дурака'''
+""" Мультиагентные среды для игры в дурака """
 
 
 # %% imports 
@@ -10,7 +10,7 @@ from gym.core import Env
 
 # %% constants
 class SUIT(Enum):
-    '''Масти карт'''
+    """ Масти карт """
     SPADES = auto()
     HEARTS = auto()
     CLUBS = auto()
@@ -18,56 +18,65 @@ class SUIT(Enum):
 
 
 class TURN_TYPE(Enum):
-    '''Типы хода'''
+    """ Типы хода """
+    START = auto()
+    """ Атака на первом ходу """
     ATTACK = auto()
-    '''Ходит на...'''
+    """ Ходит на... """
     DEFENSE = auto()
-    '''Отбивается'''
+    """ Отбивается """
     ATTACK_END = auto()
-    '''Дает карты в догонку'''
+    """ Дает карты в догонку """
     DEFENSE_END = auto()
-    '''Не может побиться, берет карты'''
+    """ Не может побиться, берет карты """
 
 
 class ACTION_TYPE(Enum):
-    '''Типы действий'''
+    """ Типы действий """
     PUT = auto()
-    '''Положить карту или побиться картой'''
+    """ Положить карту или побиться картой """
     TAKE = auto()
-    '''Взять карты после неудачной попытки отбиться'''
+    """ Взять карты после неудачной попытки отбиться """
     FINISH = auto()
-    '''Закончить давать карты'''
+    """ Закончить давать карты """
 
 
+class UnknownTransitionError(Exception):
+    pass
+
+
+
+# %% classes
 class Turn_2a:
-    '''Отслеживает, чей сейчас ход и какой тип хода.
+    """ Отслеживает, чей сейчас ход и какой тип хода.
     
-    .. warning:: Не проверяет корректность действий игроков!'''
+    .. warning:: Не проверяет корректность действий игроков! """
     def __init__(self):
         self.reset()
     
     def reset(self):
-        '''Начинает всегда первый игрок'''
+        """ Начинает всегда первый игрок """
         self._turn = [TURN_TYPE.ATTACK, TURN_TYPE.DEFENSE]
         self._player = 0
         self._other_player = 1
     
     def swap(self):
-        '''Меняет поля, отслеживающие, кто ходит'''
+        """ Смена хода
+        Меняет поля, отслеживающие, кто ходит """
         self._player, self._other_player = self._other_player, self._player
 
     def new_round(self, swap: bool=False):
-        '''Начинает новый раунд
+        """ Начинает новый раунд
         
         :param swap: нужно ли менять атакующего
-        '''
+        """
         if swap:
             self.swap()
         self._turn[self._player] = TURN_TYPE.ATTACK
         self._turn[self._other_player] = TURN_TYPE.DEFENSE
 
     def turn(self, action_type: ACTION_TYPE):
-        '''Переход в новое состояние'''
+        """ Переход в новое состояние """
         # Если атакующий кладет новую карту:
         if action_type is ACTION_TYPE.PUT:
             self.swap()
@@ -86,21 +95,71 @@ class Turn_2a:
                 self.new_round(swap=True)
     
     def reverse_and_swap(self):
-        '''Меняет состояния игроков
-        и поля, отслеживающие, кто ходит.'''
+        """ Меняет состояния игроков
+        и делает смену хода. """
         self._turn.reverse()
         self.swap()
 
 
+class Turn_2a_v1:
+    """ Отслеживает, чей сейчас ход и какой тип хода.
+    
+    .. warning:: Не проверяет корректность действий игроков! """
+    
+    _transitions = [
+        (ACTION_TYPE.PUT, TURN_TYPE.START, TURN_TYPE.DEFENSE, True),
+        (ACTION_TYPE.PUT, TURN_TYPE.ATTACK, TURN_TYPE.DEFENSE, True),
+        (ACTION_TYPE.PUT, TURN_TYPE.DEFENSE, TURN_TYPE.ATTACK, True),
+        (ACTION_TYPE.PUT, TURN_TYPE.ATTACK_END, TURN_TYPE.ATTACK_END, False),
+        (ACTION_TYPE.TAKE, TURN_TYPE.DEFENSE, TURN_TYPE.ATTACK_END, True),
+        (ACTION_TYPE.FINISH, TURN_TYPE.ATTACK, TURN_TYPE.START, True),
+        (ACTION_TYPE.FINISH, TURN_TYPE.ATTACK_END, TURN_TYPE.START, False)
+    ]
+    """ action, state, new_state, swap_or_not """
+
+    def __init__(self):
+        self.reset()
+    
+    def reset(self):
+        """ Начинает всегда первый игрок """
+        self._player = 0
+        self._turn = TURN_TYPE.START
+    
+    def swap(self):
+        """ Смена хода
+        Меняет поля, отслеживающие, кто ходит """
+        self._player = int(not self._player)
+
+    def new_round(self, swap: bool=False):
+        """ Начинает новый раунд
+        
+        :param swap: нужно ли менять атакующего
+        """
+        self.change(TURN_TYPE.START, swap)
+
+    def change(self, new_turn, swap: bool=False):
+        if swap:
+            self.swap()
+        self._turn = new_turn
+
+    def turn(self, action_type: ACTION_TYPE):
+        """ Переход в новое состояние """
+        for trans_action_type, source, dest, swap in self._transitions:
+            if (action_type is trans_action_type and self._turn is source):
+                if swap:
+                    self.swap()
+                self._turn = dest
+                return
+        
+        raise UnknownTransitionError(
+            f"Unknown action <{action_type}> for turn <{self._turn}>")
 
 
-
-# %% classes
 class Durak_2a_v0:
-    '''Среда для игры в Дурака вдвоем
+    """ Среда для игры в Дурака вдвоем
     
     Игроки ходят по очереди.
-    '''
+    """
 
     def __init__(self):
         pass
